b88924d9f90e0256ac1918837c594883a02aca27
Naama133 <naamab@wix.com>
2024-03-27 13:39:43 +0200

    Use context to share values

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { CartContext } from "../store/shopping-cart-context";
 import { useContext } from "react";
 
-export default function Cart({ onUpdateItemQuantity }) {
+export default function Cart() {
 
   const cartCtx = useContext(CartContext);
 
@@ -26,11 +26,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => cartCtx.updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => cartCtx.updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
